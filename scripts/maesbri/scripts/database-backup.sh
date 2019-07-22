03b81141fd0caa1555e1384804dfda3621bc8095
maesbri <maesbri@gmail.com>
2019-07-22 11:49:36 +0200

    moved to bash folder

--- scripts/maesbri/scripts/database-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Let's export the password for 'clarity' database user so it is not asked everytime we use the psql command
-export PGPASSWORD='IMHnl8ORQcuNtaujfY0V84BM6TWovROt'
-
-pg_dump -U clarity -h localhost -d claritydb | gzip -9 | split -b 250M - claritydb.pgsql.gz
