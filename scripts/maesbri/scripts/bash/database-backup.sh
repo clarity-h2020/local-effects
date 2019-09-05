510f48ae2b38cb68ec988260f8a6e006c79bfaa0
maesbri <maesbri@gmail.com>
2019-09-05 10:15:17 +0200

    replaced "source" by "." (it is the same, but "." is less problematic with some shells)

--- scripts/maesbri/scripts/bash/database-backup.sh
+++ scripts/maesbri/scripts/bash/database-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source ./env.sh
+. ./env.sh
 
-pg_dump -U ${PGUSER} -h ${PGHOST} -p ${PGPORT} -d ${PGDATABASE} | gzip -9 | split -b 250M - ${DATA_ROOT}'/database-backup/'${PGDATABASE}.pgsql.gz
+pg_dump -U ${PGUSER} -h ${PGHOST} -p ${PGPORT} -d ${PGDATABASE} | gzip -9 | split -b 500M - ${DATA_ROOT}'/database-backup/'${PGDATABASE}.pgsql.gz
--- scripts/maesbri/scripts/bash/database-restore.sh
+++ scripts/maesbri/scripts/bash/database-restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source ./env.sh
+. ./env.sh
 
 cat ${DATA_ROOT}'/database-backup/'${PGDATABASE}.pgsql.gz* | gunzip | psql -U ${PGUSER} -h ${PGHOST} -p ${PGPORT} -d ${PGDATABASE}
