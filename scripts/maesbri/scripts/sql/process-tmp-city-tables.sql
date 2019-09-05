f85fed82f3455b58a9cd1565be001813568d1698
maesbri <maesbri@gmail.com>
2019-09-05 19:56:24 +0200

    updated the way how urban atlas layers are processed. Finished also with water layer processing

--- scripts/maesbri/scripts/sql/process-tmp-city-tables.sql
+++ scripts/maesbri/scripts/sql/process-tmp-city-tables.sql
@@ -1,11 +1,15 @@
+UPDATE __urban_atlas SET geom=St_MakeValid(geom);
+WITH deleted_geometries AS (DELETE FROM __urban_atlas WHERE NOT ST_Isvalid(geom) IS TRUE RETURNING *) SELECT count(*) FROM deleted_geometries;
 
---- insert in the city table the basic information about the current city being processed
-INSERT INTO city (name, code, country_code, boundary, bbox) 
-       VALUES (UPPER(:city_name), :city_code, :country_code, ST_GeomFromText(:city_boundary), ST_Envelope(ST_GeomFromText(:city_boundary)));
 
---- define a temporary variable "city_grid" with the LAEA grid cells that intersect with the city bbox
+INSERT INTO city (name, code, country_code, population, boundary, bbox)
+        (SELECT c.name, c.code, c.country_code, c.population, c.wkb_geometry, ST_Envelope(c.wkb_geometry) FROM __city c WHERE c.code = :city_code);
+
+--- define a temporary variable "city_grid" with the LAEA grid cells that intersect with the city boundary
 DROP TABLE IF EXISTS __city_grid;
-CREATE TEMPORARY TABLE __city_grid AS (SELECT g.fid, g.geom FROM city c, laea_etrs_500m g WHERE ST_Intersects(c.bbox, g.geom));
+CREATE TEMPORARY TABLE __city_grid AS (SELECT g.fid, g.geom FROM city c, laea_etrs_500m g WHERE c.code = :city_code AND ST_Intersects(c.boundary, g.geom));
+
+---LAYERS=("water" "roads" "railways" "trees" "vegetation" "agricultural_areas" "built_up" "built_open_spaces" "dense_urban_fabric" "medium_urban_fabric" "low_urban_fabric" "public_military_industrial")
 
 --- WATER LAYER ---
 --- save in the cityid variable the value assigned by the database in the "city" table
@@ -14,11 +18,19 @@ INSERT INTO __water (cityid, cellid, geom)
        (SELECT cityid.id,
                cg.fid,
                ST_Multi(ST_CollectionExtract(ST_Intersection(ST_MakeValid(ST_SnapToGrid(ST_Union(ua.geom),0.0001)), cg.geom),3)) AS geom
-               FROM cityid, __city_grid cg, __urban_atlas ua WHERE ua.featuretype_code = '50000' AND ST_Intersects(ua.geom, cg.geom) GROUP BY cg.geom,cg.fid,cityid.id);
+               FROM cityid, __city_grid cg, __urban_atlas ua WHERE ua.featuretype_code IN ('50000') AND ST_Intersects(ua.geom, cg.geom) GROUP BY cg.geom,cg.fid,cityid.id);
 
-FUA_TUNNEL=`grep -i -F ['dense_urban_fabric'] $PARAMETERS/fua_tunnel.dat | cut -f 2 -d ' '`
-UPDATE __water w SET fua_tunnel="$FUA_TUNNEL" FROM __urban_atlas ua WHERE ua.featuretype_code IN ['11100', '11210'] AND ST_Intersects(w.geom , ua.geom );"
-FUA_TUNNEL=`grep -i -F ['medium_urban_fabric'] $PARAMETERS/fua_tunnel.dat | cut -f 2 -d ' '`
-psql -U "postgres" -d "clarity" -c "UPDATE public.\""$NAME"\" x SET fua_tunnel="$FUA_TUNNEL" FROM "$CITY"_layers9_12 l WHERE "$CODE"='11220' AND ST_Intersects( x.geom , l.geom );"
+UPDATE __water w SET fua_tunnel=get_parameter_value('fua_tunnel', 'dense_urban_fabric') FROM __urban_atlas ua WHERE ua.featuretype_code IN ('11100', '11210') AND ST_Intersects(w.geom , ua.geom );
+UPDATE __water w SET fua_tunnel=get_parameter_value('fua_tunnel', 'medium_urban_fabric') FROM __urban_atlas ua WHERE ua.featuretype_code IN ('11220') AND ST_Intersects(w.geom , ua.geom );
+
+UPDATE __water w SET building_shadow=0 FROM __urban_atlas ua WHERE ua.featuretype_code IN ('11100', '11210', '11220', '11230', '11240', '11300', '12100') AND ST_Intersects(w.geom , ua.geom );
 
 
+
+
+
+-- Copy __water table information to water table
+WITH cityid AS (SELECT id FROM city c WHERE c.code = :city_code)
+INSERT INTO water (cityid, cellid, geom, albedo, emissivity, transmissivity, vegetation_shadow, run_off_coefficient, fua_tunnel, building_shadow) 
+        (SELECT cityid.id, cellid, geom, albedo, emissivity, transmissivity, vegetation_shadow, run_off_coefficient, fua_tunnel, building_shadow
+        FROM cityid, __water);
